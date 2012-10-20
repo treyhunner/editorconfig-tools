0f590c3f0cb0b9a9e645a34038229d1923584e23
Trey Hunner <treyhunner@gmail.com>
2012-10-19 21:07:45 -0700

    Refactor indent_finder and gut verbose/vim options

--- indent_finder.py
+++ indent_finder.py
@@ -21,10 +21,8 @@
 import sys
 import re
 
-VERBOSE = 0
-
 help = \
-"""Usage : %s [ --separate ] [ --vim-output ] [ --verbose ] file1 file2 ... fileN
+"""Usage : %s [ --separate ] [ --verbose ] file1 file2 ... fileN
 
 Display indentation used in the list of files. Possible answers are (with X
 being the number of spaces used for indentation):
@@ -35,16 +33,10 @@ tab 8
 file1: space X
 file2: tab 8
 
---vim-output: output suitable to use inside vim:
-set sts=0 | set tabstop=4 | set noexpandtab | set shiftwidth=4
-
 """
 
 ### used when it can not compute the exact value of the indentation
 default = ("space", 4)
-
-### Used when indentation is tab, to set tabstop
-vim_preferred_tabstop_value = 4
 
 
 class IndentFinder:
@@ -84,6 +76,8 @@ class IndentFinder:
     mail, if possible with the offending file.
     """
 
+    INDENT_RE = re.compile(r"^((\s)\2*)\S")
+
     def __init__(self):
         self.clear()
 
@@ -92,124 +86,79 @@ class IndentFinder:
             self.parse_file(fname)
 
     def parse_file(self, fname):
-        f = open(fname)
-        l = f.readline()
-        while(l):
-            self.analyse_line(l)
-            l = f.readline()
-        f.close()
+        with open(fname) as f:
+            for line in f:
+                self.analyse_line(line)
 
     def clear(self):
         self.spaces = [0, 0, 0, 0, 0, 0, 0]  # 2-8 entries
-        self.tab = 0
+        self.tabbed_lines = 0
         self.last_space_indent = 0
-        self.nb_lines = 0
-        self.nb_skipped_lines = 0
-        self.indent_re = re.compile(r"^((\s)\2*)\S")
+        self.total_lines = 0
+        self.skipped_lines = 0
         self.skip_line = 0
 
     def analyse_line(self, line):
-        self.nb_lines += 1
-        skip_line = self.skip_line
-        if len(line) > 2 and line[-2] == "\\":
-            self.skip_line = 1
-        else:
-            self.skip_line = 0
-        if skip_line:
-            self.nb_skipped_lines += 1
+        self.total_lines += 1
+        skip_this_line = self.skip_line
+        self.skip_line = (len(line) > 2 and line[-2] == "\\")
+        if skip_this_line:
+            self.skipped_lines += 1
             return
 
-        mo = self.indent_re.match(line)
-        if mo:
-            if mo.group(2) == '\t':
-                self.tab += 1
+        match = self.INDENT_RE.match(line)
+        if match:
+            if match.group(2) == '\t':
+                self.tabbed_lines += 1
                 self.last_space_indent = 0
                 return
-            nb_space = len(mo.group(1))
+            num_spaces = len(match.group(1))
             for i in range(2, 9):
-                if (nb_space % i) == 0:
+                if (num_spaces % i) == 0:
                     self.spaces[i - 2] += 1
-                if self.last_space_indent and (nb_space - self.last_space_indent) == i:
+                if (self.last_space_indent and
+                    num_spaces - self.last_space_indent == i):
                     # bonus when we detect that a new indentation level was reached
                     self.spaces[i - 2] += 10
-            self.last_space_indent = nb_space
-            return
+            self.last_space_indent = num_spaces
         else:
-            self.nb_skipped_lines += 1
+            self.skipped_lines += 1
 
     def results(self):
-        if VERBOSE:
-            print "Nb of scanned lines : %d" % self.nb_lines
-            print "Nb of ignored lines : %d" % self.nb_skipped_lines
-            print "Nb of lines with tab indentation: %d" % self.tab
-            for i in range(2, 9):
-                print "Nb of points for space %d indentation: %d" % (i, self.spaces[i - 2])
-
-        if self.tab > max(self.spaces):
-            if VERBOSE:
-                print "Result = tab"
+        if self.tabbed_lines > max(self.spaces):
             return ("tab", 8)
 
-        nb = 0
-        idx = -1
+        num_lines = 0
+        indent_size = -1
         for i in range(8, 1, -1):
-            if self.spaces[i - 2] > int(nb * 1.1):  # give a 10% threshold
-                idx = i
-                nb = self.spaces[idx - 2]
+            if self.spaces[i - 2] > int(num_lines * 1.1):  # give a 10% threshold
+                indent_size = i
+                num_lines = self.spaces[indent_size - 2]
 
-        if idx == -1:  # no lines
-            raise Exception("<Empty file>")
+        if indent_size == -1:  # no indentation
+            return (None, None)
 
-        if VERBOSE:
-            print "Result = space, %d" % idx
-        return ("space", idx)
+        return ("space", indent_size)
 
     def __str__(self):
-        try:
-            return "%s %d" % self.results()
-        except Exception:
+        indent_style, indent_size = self.results()
+        if indent_style == 'space':
+            return "space %d" % indent_size
+        elif indent_style == 'tab':
+            return "tab"
+        else:
             return "<Empty file>"
-
-    def vim_output(self):
-        try:
-            ts, n = self.results()
-        except Exception:
-            return '" Empty file'
-        # spaces:
-        #     => set sts to the number of spaces
-        #   => set tabstop to the number of spaces
-        #   => expand tabs to spaces
-        #   => set shiftwidth to the number of spaces
-        if ts == "space":
-            return "set sts=%d | set tabstop=%d | set expandtab | set shiftwidth=%d" % (n, n, n)
-
-        tab_width = 4
-        # tab:
-        #   => set sts to 0
-        #   => set tabstop to preferred value
-        #   => set expandtab to false
-        #   => set shiftwidth to tabstop
-        #   tabstop should not be touched.
-        if ts == "tab":
-            return "set sts=0 | set tabstop=%d | set noexpandtab | set shiftwidth=%d" % (vim_preferred_tabstop_value, tab_width)
 
 
 def main():
-    global VERBOSE
 
     SEPARATE = 0
-    VIM_OUTPUT = 0
-    VERBOSE = 0
 
     fi = IndentFinder()
     file_list = []
     for opt in sys.argv[1:]:
         if opt == "--separate":
             SEPARATE = 1
-        elif opt == "--vim-output":
-            VIM_OUTPUT = 1
-        elif opt == "--verbose":
-            VERBOSE = 1
         elif opt[0] == "-":
             print help % sys.argv[0]
             return
@@ -224,10 +173,7 @@ def main():
         return
 
     fi.parse_file_list(file_list)
-    if VIM_OUTPUT:
-        print fi.vim_output()
-    else:
-        print str(fi)
+    print str(fi)
 
 
 if __name__ == "__main__":
