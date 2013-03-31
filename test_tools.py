413f31a1d8e2e70635d32ca617e5a8b411971818
Trey Hunner <trey@treyhunner.com>
2013-03-31 11:30:49 -0700

    Add tests for charset

--- test_tools.py
+++ test_tools.py
@@ -10,9 +10,9 @@ def get_filename(test_file):
     return abspath('tests/{0}'.format(test_file))
 
 
-class EditorConfigCheckerTest(TestCase):
+class EditorConfigTestCase(TestCase):
 
-    """Tests for EditorConfigChecker"""
+    """Test case for EditorConfigChecker tests"""
 
     def assertFileErrors(self, filename, expected_errors):
         full_filename = get_filename(filename)
@@ -20,6 +20,11 @@ class EditorConfigCheckerTest(TestCase):
         checker = EditorConfigChecker()
         errors = checker.check(full_filename, props)
         self.assertEquals((filename, errors), (filename, expected_errors))
+
+
+class LineEndingTests(EditorConfigTestCase):
+
+    """Tests for EditorConfigChecker line endings"""
 
     def test_check_crlf(self):
         self.assertFileErrors('crlf_valid.txt', [])
@@ -49,4 +54,74 @@ class EditorConfigCheckerTest(TestCase):
         self.assertFileErrors('lf_invalid_crlf.txt', [
             "Incorrect line ending found: crlf",
             "No final newline found",
+        ])
+
+
+class CharsetTests(EditorConfigTestCase):
+
+    """Tests for EditorConfigChecker charsets"""
+
+    def test_utf8(self):
+        self.assertFileErrors('utf-8_valid.txt', [])
+        self.assertFileErrors('utf-8_valid_latin1.txt', [])
+        self.assertFileErrors('utf-8_invalid_utf-8-bom.txt', [
+            "Charset utf-8-bom found",
+        ])
+        self.assertFileErrors('utf-8_invalid_utf-16be.txt', [
+            "Charset utf-16be found",
+        ])
+        self.assertFileErrors('utf-8_invalid_utf-16le.txt', [
+            "Charset utf-16le found",
+        ])
+
+    def test_utf8_with_bom(self):
+        self.assertFileErrors('utf-8-bom_valid.txt', [])
+        self.assertFileErrors('utf-8-bom_invalid_latin1.txt', [
+            "Charset utf-8 or latin1 found",
+        ])
+        self.assertFileErrors('utf-8-bom_invalid_utf-8.txt', [
+            "Charset utf-8 or latin1 found",
+        ])
+        self.assertFileErrors('utf-8-bom_invalid_utf-16be.txt', [
+            "Charset utf-16be found",
+        ])
+        self.assertFileErrors('utf-8-bom_invalid_utf-16le.txt', [
+            "Charset utf-16le found",
+        ])
+
+    def test_latin1(self):
+        self.assertFileErrors('latin1_valid.txt', [])
+        self.assertFileErrors('latin1_invalid_utf-8.txt', [])
+        self.assertFileErrors('latin1_invalid_utf-8-bom.txt', [])
+        self.assertFileErrors('latin1_invalid_utf-16be.txt', [])
+        self.assertFileErrors('latin1_invalid_utf-16le.txt', [])
+
+    def test_utf16be(self):
+        self.assertFileErrors('utf-16be_valid.txt', [])
+        self.assertFileErrors('utf-16be_invalid_utf-16le.txt', [
+            "Charset utf-16le found",
+        ])
+        self.assertFileErrors('utf-16be_invalid_utf-8.txt', [
+            "Charset utf-8 or latin1 found",
+        ])
+        self.assertFileErrors('utf-16be_invalid_utf-8-bom.txt', [
+            "Charset utf-8-bom found",
+        ])
+        self.assertFileErrors('utf-16be_invalid_latin1.txt', [
+            "Charset utf-8 or latin1 found",
+        ])
+
+    def test_utf16le(self):
+        self.assertFileErrors('utf-16le_valid.txt', [])
+        self.assertFileErrors('utf-16le_invalid_utf-16be.txt', [
+            "Charset utf-16be found",
+        ])
+        self.assertFileErrors('utf-16le_invalid_utf-8.txt', [
+            "Charset utf-8 or latin1 found",
+        ])
+        self.assertFileErrors('utf-16le_invalid_utf-8-bom.txt', [
+            "Charset utf-8-bom found",
+        ])
+        self.assertFileErrors('utf-16le_invalid_latin1.txt', [
+            "Charset utf-8 or latin1 found",
         ])
